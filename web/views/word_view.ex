b3c0b952d018d861035aa275fe82cf4d8cfbb488
Eito Katagiri <eitoball@gmail.com>
2016-12-27 13:58:20 +0900

    refine show word page

--- web/views/word_view.ex
+++ web/views/word_view.ex
@@ -1,3 +1,12 @@
 defmodule WikigoElixir.WordView do
   use WikigoElixir.Web, :view
+
+  def add_word_link(conn, body) do
+    WikigoElixir.Word.titles
+    |> Enum.reduce(body, fn (title, acc) ->
+      show_word_path = word_path(conn, :show, title)
+      link = link(title, to: show_word_path) |> safe_to_string
+      Regex.replace(~r/#{title}/, acc, link)
+    end)
+  end
 end
