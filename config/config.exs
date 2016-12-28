4eea95bdd8d602f307c935136088a5a69ebf6dfb
Eito Katagiri <eitoball@gmail.com>
2016-12-28 16:49:08 +0900

    disable user registration

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ config :coherence,
   logged_out_url: "/",
   email_from_name: "Your Name",
   email_from_email: "yourname@example.com",
-  opts: [:authenticatable, :recoverable, :lockable, :trackable, :unlockable_with_token, :registerable]
+  opts: [:authenticatable, :recoverable, :lockable, :trackable, :unlockable_with_token]
 
 config :coherence, WikigoElixir.Coherence.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
